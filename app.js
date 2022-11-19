11ebbf112adefd0331a0179b03113f4dd1098be8
lucascmpus <lucascmpus@gmail.com>
2022-11-19 19:08:08 -0300

    update modal

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inputAdd = document.getElementById('inputText');
 const selectStatus = document.getElementById('selectStatus');
 const rowBody = document.getElementById('tbody');
 const editInput = document.getElementById('newName');
+const editTaskModal = new bootstrap.Modal(document.getElementById('exampleModal'))
 
 var todoList = [];
 
@@ -42,42 +43,17 @@ const insertTaskWindow = (item, status, indice) => {
     <td data-indice=${indice}>${status}</td>
 
     <th>
-        <button data-indice=${indice} id="edit" type="button" class="btn btn-primary btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Editar
-        </button>
+        <button data-indice=${indice} id="edit-${indice}" type="button" class="edit btn btn-primary btn-warning btn-sm">Editar</button>
     </th>
 
     <th>
-        <button data-indice=${indice} id="delete" type="button" class="btn btn-danger btn-sm">Excluir</button>
+        <button data-indice=${indice} id="delete-${indice}" type="button" class="delete btn btn-danger btn-sm">Excluir</button>
     </th>
     ` 
     rowBody.appendChild(row)
     inputAdd.value = ''
 }
 
-
-
-
-// att status
-const attStatux = (item) =>{
-    var list = JSON.parse(localStorage.getItem('todolist'))
-    list.forEach((items) => {
-        if(items.item == item && items.status == "Fazer"){
-            items.item = item;
-            items.status = "Fazendo"
-        }
-        else if (items.item == item && items.status == "Fazendo"){
-            items.item = item;
-            items.status = "Concluido"
-        }
-        else if (items.item == item && items.status == "Concluido"){
-            items.item = item;
-            items.status = "Fazer"
-        }
-    })
-    localStorage.setItem('todolist', JSON.stringify(list))
-    loadTasks()    
-}
 
 // deleteTask and attStatus
 const deletarTarefa = (indice) => {
@@ -119,21 +95,67 @@ const attStatus = (index,textContent) =>{
 const clickItem = (e) => {
     const elemento = e.target;
     
-    if (elemento.id == 'delete'){
+    if (elemento.className.includes('delete')){
         const indice = elemento.dataset.indice;
         deletarTarefa(indice)
-    } 
-
+    }
+    
     if (elemento.tagName == 'TD'){
         const index = elemento.dataset.indice;
         const textContent = elemento.textContent;
         attStatus(index, textContent)
-    }       
+    }
+    
+    if (elemento.className.includes('edit')){
+        const indice = elemento.dataset.indice;
+        openEditTaskModal(indice)
+    }
+    
+    if(elemento.type == 'button' && elemento.textContent== "Editar"){
+                        
+        
+    }
     
 }
 
+// edit task
+
+const openEditTaskModal = (indice) => {
+    console.log(indice);
+    document.getElementById("numero-tarefa").innerText = parseInt(indice) + 1;
+    document.getElementById("indice-tarefa-edicao").value = indice;
+    editTaskModal.show();
+}
+
+document.getElementById("save-edit-task").addEventListener("click", (e) => {
+    var nomeTarefa = document.getElementById("edit-modal-nome-tarefa").value;
+    var indiceTarefa = document.getElementById("indice-tarefa-edicao").value;
+    var descricaoTarefa = "";
+    
+    var informacoesTarefa = {
+        indice: indiceTarefa,
+        nome: nomeTarefa,
+        descricao: descricaoTarefa
+    };
+    
+    editTask(informacoesTarefa);
+})
+
+const editTask = (informacoesTarefa) => {
+    var list = JSON.parse(localStorage.getItem('todolist'))
+    list.forEach((items) =>{
+        items.item = inputValue
+    })
+    localStorage.setItem('todolist', JSON.stringify(list))
+        loadTasks()
+}
+
+
+
 
 // events to add task
+
+// document.getElementById('exampleModal').addEventListener('click', clickItem)
 
 document.getElementById('table').addEventListener('click', clickItem)
 
